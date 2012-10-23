8717193cf4db4af0a46923c998a02c248fcd39ad
Peng Zhong <peng@nylira.com>
2012-10-24 00:50:18 +0900

    add use cases

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+$('ul.tabs li.tab').click(function() {
+  $('ul.tabs li.tab').removeClass('active');
+  $('ul.tab-content li.tab-content').removeClass('active');
+  $(this).addClass('active');
+  var tab_content = $(this).attr('href');
+  $(tab_content).addClass('active');
+})
