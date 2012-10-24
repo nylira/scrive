05cf4aa3aa34439bbbcfcd0cf2c47167205f51e3
Peng Zhong <peng@nylira.com>
2012-10-25 01:02:06 +0900

    update learn-more page"

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+$(function() {
+
 $('ul.tabs li.tab').click(function() {
   $('ul.tabs li.tab').removeClass('active');
   $('ul.tab-content li.tab-content').removeClass('active');
@@ -5,3 +7,7 @@ $('ul.tabs li.tab').click(function() {
   var tab_content = $(this).attr('href');
   $(tab_content).addClass('active');
 })
+
+$('select').select2();
+
+});
